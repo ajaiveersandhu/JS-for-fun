4747b18809375d0d53fdd0af248144652170bcf5
AjaiVeer Sandhu <ajaiveersandhu@gmail.com>
2021-06-21 06:48:22 -0400

    list items are getting stored in localstorage with all their properties

--- Shopping List/script.js
+++ Shopping List/script.js
@@ -2,7 +2,7 @@ const shoppingForm = document.querySelector('.shopping');
 const list = document.querySelector('.list');
 
 // maintaing state
-const items = [];
+let items = [];
 
 function formSubmit(e) {
   e.preventDefault();
@@ -15,22 +15,65 @@ function formSubmit(e) {
   };
   items.push(item);
   e.target.reset();
-
-  list.dispatchEvent(new CustomEvent('itemUpdated'));
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
 }
 
 function displayItems() {
   let html = '';
   items.forEach((item) => {
     html += `<li class="shopping-item">
-		<input type="checkbox"/>
+		<input 
+			type="checkbox" 
+			value=${item.id} 
+			${item.complete && 'checked'}
+			/>
 		<span class="itemName">${item.name}</span>
-		<button aria-label="Remove ${item.name}">&times;</button>
+		<button 
+			value=${item.id} 
+			aria-label="Remove ${item.name}">
+			&times;
+		</button>
 		</li>`;
   });
 
   list.innerHTML = html;
 }
 
+function storeDataToLocalStorage() {
+  localStorage.setItem('items', JSON.stringify(items));
+}
+
+function fetchDataFromLocalStorage() {
+  const listItems = JSON.parse(localStorage.getItem('items'));
+  if (listItems.length) items.push(...listItems);
+
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
+function deleteItem(e) {
+  if (e.target.matches('button')) {
+    items = items.filter((item) => item.id !== parseInt(e.target.value));
+    e.target.parentElement.remove();
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+  }
+}
+
+function markedComplete(e) {
+  if (e.target.matches('input[type="checkbox"]')) {
+    const completeItem = items.find(
+      (item) => item.id === parseInt(e.target.value)
+    );
+    completeItem.complete = !completeItem.complete;
+    console.log(completeItem);
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+  }
+}
+
 shoppingForm.addEventListener('submit', formSubmit);
-list.addEventListener('itemUpdated', displayItems);
+list.addEventListener('itemsUpdated', displayItems);
+list.addEventListener('itemsUpdated', storeDataToLocalStorage);
+list.addEventListener('click', (e) => {
+  deleteItem(e);
+  markedComplete(e);
+});
+window.addEventListener('load', fetchDataFromLocalStorage);
