b395b31c9a5a255d0e1d1d965bef5cbf9e3b5bcf
AjaiVeer Sandhu <ajaiveersandhu@gmail.com>
2021-06-21 03:54:49 -0400

    form submission is working

--- Shopping List/script.js
+++ Shopping List/script.js
@@ -1,1 +1,9 @@
-console.log('test');
+const shoppingForm = document.querySelector('.shopping');
+const list = document.querySelector('.list');
+
+function formSubmit(e) {
+  e.preventDefault();
+  console.log('Submitted');
+}
+
+shoppingForm.addEventListener('submit', formSubmit);
