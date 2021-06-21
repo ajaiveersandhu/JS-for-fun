ad92cf163c4467fb3ef9c83561c04a96e7a5c9b6
AjaiVeer Sandhu <ajaiveersandhu@gmail.com>
2021-06-21 02:08:00 -0400

    functions are trigger based on the input in textarea

--- Text Generator/script.js
+++ Text Generator/script.js
@@ -1,1 +1,22 @@
-console.log('testS');
+const filters = Array.from(document.querySelectorAll('[name="filter"]'));
+const textArea = document.querySelector('[name="text"]');
+const result = document.querySelector('.result');
+
+const filterOptions = {
+  sarcastic() {
+    console.log('sarcastic');
+  },
+  funky() {
+    console.log('funky');
+  },
+  unable() {
+    console.log('unbal');
+  },
+};
+
+function transforText(text) {
+  const filter = filters.find((input) => input.checked).value;
+  const changedText = Array.from(text).map(filterOptions[filter]);
+}
+
+textArea.addEventListener('input', (e) => transforText(e.target.value));
