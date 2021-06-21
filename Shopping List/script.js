11ed5f0344fb4dcd2d981b252125793c7c85696d
AjaiVeer Sandhu <ajaiveersandhu@gmail.com>
2021-06-21 04:41:59 -0400

    list updating with custom event

--- Shopping List/script.js
+++ Shopping List/script.js
@@ -1,9 +1,36 @@
 const shoppingForm = document.querySelector('.shopping');
 const list = document.querySelector('.list');
 
+// maintaing state
+const items = [];
+
 function formSubmit(e) {
   e.preventDefault();
-  console.log('Submitted');
+  const name = e.currentTarget.item.value;
+  if (!name) return;
+  const item = {
+    name,
+    id: Date.now(),
+    complete: false,
+  };
+  items.push(item);
+  e.target.reset();
+
+  list.dispatchEvent(new CustomEvent('itemUpdated'));
+}
+
+function displayItems() {
+  let html = '';
+  items.forEach((item) => {
+    html += `<li class="shopping-item">
+		<input type="checkbox"/>
+		<span class="itemName">${item.name}</span>
+		<button aria-label="Remove ${item.name}">&times;</button>
+		</li>`;
+  });
+
+  list.innerHTML = html;
 }
 
 shoppingForm.addEventListener('submit', formSubmit);
+list.addEventListener('itemUpdated', displayItems);
