6be5c59fe7d4ab15d99c5062468fa49ae772157f
AjaiVeer Sandhu <ajaiveersandhu@gmail.com>
2021-06-21 23:16:23 -0400

    next and previous buttons working

--- Gallery/script.js
+++ Gallery/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 function Gallery(gallery) {
   if (!gallery) throw new Error('No Gallery Found!');
 
@@ -7,17 +8,36 @@ function Gallery(gallery) {
   const prevButton = document.querySelector('.prev');
   let currentElement;
 
+  function closeModal() {
+    modal.classList.remove('open');
+    // removing eventListners
+    window.removeEventListener(
+      'keyup',
+      (e) => e.key === 'Escape' && closeModal()
+    );
+
+    // next and prev button
+    nextButton.removeEventListener('click', () => {
+      console.log(currentElement);
+    });
+  }
+
   function openModal() {
-    console.log('test');
     if (modal.matches('.open')) {
-      console.info('modal already open');
       return;
     }
     modal.classList.add('open');
-  }
+    // event listeners to be bound when the modal opens
+    // closing modal with esc key
+    window.addEventListener('keyup', (e) => e.key === 'Escape' && closeModal());
 
-  function closeModal() {
-    modal.classList.remove('open');
+    // next and prev button
+    nextButton.addEventListener('click', () => {
+      showImage(currentElement.nextElementSibling);
+    });
+    prevButton.addEventListener('click', () => {
+      showImage(currentElement.previousElementSibling);
+    });
   }
 
   function showImage(imageElement) {
@@ -33,15 +53,18 @@ function Gallery(gallery) {
     openModal();
   }
 
-  // event listener
+  // event listener  -----
+
+  // opening image in modal
   images.forEach((image) =>
     image.addEventListener('click', (e) => showImage(e.currentTarget))
   );
+
+  // closing modal with click
   modal.addEventListener(
     'click',
     (e) => e.target === e.currentTarget && closeModal()
   );
-  window.addEventListener('keyup', (e) => e.key === 'Escape' && closeModal());
 }
 
 const gallery1 = Gallery(document.querySelector('.gallery1'));
